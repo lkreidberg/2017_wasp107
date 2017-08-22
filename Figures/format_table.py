418479ad3099abbf1f20e107a26dda87aff1da63
Laura Kreidberg <vivacelaura@gmail.com>
2017-08-22 13:05:33 -0400

    final version?

--- Figures/format_table.py
+++ Figures/format_table.py
@@ -1,6 +1,7 @@
 import numpy as np
 
 d = np.genfromtxt("w107_spectrum_06-22-17.txt")
+l = np.genfromtxt("W107_22bins.dat", skip_header = 17)
 
 n = len(d[:,0])
 bins = np.linspace(1.12, 1.65, 21)
@@ -9,4 +10,5 @@ print np.mean(d[:,1])
 for i in range(n):
 #	print '{0:0.3f}'.format(d[i,0]), '&', '{0:0.0f}'.format((d[i,1] - np.mean(d[:,1]))*1.e6), '&', '{0:0.0f}'.format(d[i,2]*1e6), '\\\\' 
 	#print '{0:0.3f}'.format(d[i,0]), '&', '{0:0.6f}'.format(d[i,1]), '&', '{0:0.1e}'.format(d[i,2]), '\\\\' 
-	print '{0:0.3f}'.format(bins[i]), '--', '{0:0.3f}'.format(bins[i+1]), '&', '{0:0.6f}'.format(d[i,1]), '&', '{0:0.1e}'.format(d[i,2]), '\\\\' 
+#	print '{0:0.3f}'.format(bins[i]), '--', '{0:0.3f}'.format(bins[i+1]), '&', '{0:0.6f}'.format(d[i,1]), '&', '{0:0.1e}'.format(d[i,2]), '\\\\' 
+	print '{0:0.3f}'.format(l[i,0]), '--', '{0:0.3f}'.format(l[i+1,0]), '&', '{0:0.6f}'.format(d[i,1]), '&', '{0:0.1e}'.format(d[i,2]), '&', '{0:0.2f}'.format(l[i,3]), '&', '{0:0.2f}'.format(l[i,4]), '\\\\' 
